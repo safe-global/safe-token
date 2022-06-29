44695df0bd5568d40e20f64661d032af42713cdf
Tobias Schubotz <tobias.schubotz@gmail.com>
2022-06-29 16:10:49 +0200

    Update reward script

--- user-airdrop/value/scripts/fetch_rewards.py
+++ user-airdrop/value/scripts/fetch_rewards.py
@@ -1,9 +1,8 @@
-# This script fetches block rewards 
+# This script fetches block rewards from Etherscan.
+# This includes uncle rewards.
 
 from datetime import datetime
 import csv
-from time import sleep
-from requests.exceptions import HTTPError
 
 from etherscan.accounts import Account
 from etherscan.client import EmptyResponse
@@ -13,39 +12,65 @@ ETHERSCAN_API_KEY = ''
 BLOCK_TYPES = ['blocks', 'uncles']
 
 MINER_SAFES = ['0xf20b338752976878754518183873602902360704']  # Use https://dune.com/queries/638270 to get this list.
-OUTPUT_FILENAME = '../csv/rewards.csv'
+OUTPUT_FILENAME_SQL = '../csv/rewards.sql'
+OUTPUT_FILENAME_CSV = '../csv/rewards.csv'
 page_size = 10000  # Etherscan page size
 
-with open(OUTPUT_FILENAME, 'w') as outfile:
-    writer = csv.writer(outfile, delimiter=',')
+data = {}
 
-    for address in MINER_SAFES:
-        etherscan_api = Account(address=address, api_key=ETHERSCAN_API_KEY)
+# Fetch data
+for address in MINER_SAFES:
+    etherscan_api = Account(address=address, api_key=ETHERSCAN_API_KEY)
 
-        for block_type in BLOCK_TYPES:
-            page = 1
-            while True:
-                print('Safe: {}, block type: {}, page: {}'.format(address, block_type, page))
-                try:
-                    rewards = etherscan_api.get_blocks_mined_page(  
-                        blocktype=block_type, 
-                        page=page,
-                        offset=page_size)
-                except EmptyResponse:
-                    break
+    data[address] = {}
+
+    for block_type in BLOCK_TYPES:
+        page = 1
+        while True:
+            print('Safe: {}, block type: {}, page: {}'.format(address, block_type, page))
+            try:
+                rewards = etherscan_api.get_blocks_mined_page(  
+                    blocktype=block_type, 
+                    page=page,
+                    offset=page_size)
+            except EmptyResponse:
+                break
+            
+            if len(rewards) == 0:
+                break
                 
-                if len(rewards) == 0:
-                    break
-                    
-                for reward in rewards:
-                    block_number = int(reward['blockNumber'])
+            for reward in rewards:
+                block_number = int(reward['blockNumber'])
 
-                    if block_number < 6775227 or block_number > 14168325:
-                        continue
+                if block_number < 6775227 or block_number > 14168325:
+                    continue
 
-                    
-                    day = datetime.fromtimestamp(int(reward['timeStamp']))
-                    balance_change = reward['blockReward']
-                    writer.writerow(['\\{}'.format(address[1:]), str(day), balance_change])
+                day = str(datetime.fromtimestamp(int(reward['timeStamp'])).date())
+                balance_change = int(reward['blockReward'])
 
-                page += 1
+                if not day in data[address]:
+                    data[address][day] = 0
+                
+                data[address][day] += balance_change
+                
+                
+
+            page += 1
+
+# Output for usage in SQL
+with open(OUTPUT_FILENAME_SQL, 'w') as outfile:
+    
+    for address in data.keys():
+        values = []
+        for day in data[address].keys():
+            values.append("\t('\\{}'::bytea, '{}'::date,{})".format(address[1:], day, data[address][day]))
+    outfile.write('SELECT * FROM (VALUES \n')
+    outfile.write(",\n".join(values))
+    outfile.write('\n) AS t (address,day,balance_change)')
+
+# Output as CSV
+with open(OUTPUT_FILENAME_CSV, 'w') as outfile:
+    writer = csv.writer(outfile, delimiter=',')
+    for address in data.keys():
+        for day in data[address].keys():
+            writer.writerow(['\\{}'.format(address[1:]), day, data[address][day]])
